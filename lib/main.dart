5f565ca56d1423d19566c6dd2eb9c9558577aa68
jkmosa01 <35510468+jkmosa01@users.noreply.github.com>
2020-01-06 20:24:24 -0700

    added nextScramble, and some dynamic adjustments

--- lib/main.dart
+++ lib/main.dart
@@ -110,6 +110,56 @@ class _HomeState extends State<Home> {
     });
   }
 
+  void newScramble(){
+    setState((){
+      scramble = scrambler.newScramble();
+    });
+  }
+
+  void stopTimer(){
+    isSolving = false;
+    mainStopwatch.stop();
+    mainTimer = null;
+    hasBeenReleased = false;
+    newScramble();
+  }
+
+  void startFreeze(){
+    isFreezing = true;
+    freezingStopwatch.reset();
+    freezingStopwatch.start();
+    setState((){});
+    freezingTimer = Timer.periodic(Duration(milliseconds: 1),(Timer timer){
+      setState((){});
+    });
+  }
+
+  void startInspection(){
+    isInspecting = true;
+    inspectingStopwatch.reset();
+    inspectingStopwatch.start();
+    setState((){});
+    inspectingTimer = Timer.periodic(Duration(seconds: 1), (Timer timer){
+      setState((){});
+    });
+  }
+
+  void startTimer(){
+    isSolving = true;
+    isInspecting = false;
+    isFreezing = false;
+    inspectingStopwatch.stop();
+    freezingStopwatch.stop();
+    inspectingTimer = null;
+    mainStopwatch.reset();
+    mainStopwatch.start();
+    mainTimer = Timer.periodic(Duration(milliseconds: 1), (Timer timer){
+      setState((){});
+    });
+  }
+
+
+
   final FocusNode keyboardFocusNode = FocusNode();
   @override
   Widget build(BuildContext context) {
@@ -119,46 +169,21 @@ class _HomeState extends State<Home> {
       onKey: (RawKeyEvent e){
         if(e.runtimeType==RawKeyDownEvent){
           if(isSolving){
-            isSolving = false;
-            mainStopwatch.stop();
-            mainTimer = null;
-            hasBeenReleased = false;
+            stopTimer();
           }
           else if(e.logicalKey == LogicalKeyboardKey.space){
             if (!isSolving && (isInspecting || !hasInspection) && !isFreezing){
-              isFreezing = true;
-              freezingStopwatch.reset();
-              freezingStopwatch.start();
-              setState((){});
-              freezingTimer = Timer.periodic(Duration(milliseconds: 1),(Timer timer){
-                setState((){});
-              });
+              startFreeze();
             }
           }
         }
         else if(e.runtimeType==RawKeyUpEvent){
           if(hasBeenReleased && e.logicalKey == LogicalKeyboardKey.space){
             if(!isInspecting && hasInspection){
-              isInspecting = true;
-              inspectingStopwatch.reset();
-              inspectingStopwatch.start();
-              setState((){});
-              inspectingTimer = Timer.periodic(Duration(seconds: 1), (Timer timer){
-                setState((){});
-              });
+              startInspection();
             }
             else if(!isSolving && (isInspecting || !hasInspection) && isFreezing && freezingStopwatch.elapsedMilliseconds>=freezeTime){
-              isSolving = true;
-              isInspecting = false;
-              isFreezing = false;
-              inspectingStopwatch.stop();
-              freezingStopwatch.stop();
-              inspectingTimer = null;
-              mainStopwatch.reset();
-              mainStopwatch.start();
-              mainTimer = Timer.periodic(Duration(milliseconds: 1), (Timer timer){
-                setState((){});
-              });
+              startTimer();
             }
             else if(isFreezing  && freezingStopwatch.elapsedMilliseconds<freezeTime){
               isFreezing = false;
@@ -193,19 +218,10 @@ class _HomeState extends State<Home> {
           onTapDown: (TapDownDetails details){
             if(!kIsWeb){
               if(isSolving){
-                isSolving = false;
-                mainStopwatch.stop();
-                mainTimer = null;
-                hasBeenReleased = false;
+                stopTimer();
               }
               else if (!isSolving && (isInspecting || !hasInspection) && !isFreezing){
-                isFreezing = true;
-                freezingStopwatch.reset();
-                freezingStopwatch.start();
-                setState((){});
-                freezingTimer = Timer.periodic(Duration(milliseconds: 1),(Timer timer){
-                  setState((){});
-                });
+                startFreeze();
               }
             }
 
@@ -213,26 +229,10 @@ class _HomeState extends State<Home> {
           onTapUp: (TapUpDetails details){//TOOD: Dragging finger doesn't work well
             if(hasBeenReleased && !kIsWeb){
               if(!isInspecting && hasInspection){
-                isInspecting = true;
-                inspectingStopwatch.reset();
-                inspectingStopwatch.start();
-                setState((){});
-                inspectingTimer = Timer.periodic(Duration(seconds: 1), (Timer timer){
-                  setState((){});
-                });
+                startInspection();
               }
               else if(!isSolving && (isInspecting || !hasInspection) && isFreezing && freezingStopwatch.elapsedMilliseconds>=freezeTime){
-                isSolving = true;
-                isInspecting = false;
-                isFreezing = false;
-                inspectingStopwatch.stop();
-                freezingStopwatch.stop();
-                inspectingTimer = null;
-                mainStopwatch.reset();
-                mainStopwatch.start();
-                mainTimer = Timer.periodic(Duration(milliseconds: 1), (Timer timer){
-                  setState((){});
-                });
+                startTimer();
               }
               else if(isFreezing  && freezingStopwatch.elapsedMilliseconds<freezeTime){
                 isFreezing = false;
@@ -244,65 +244,68 @@ class _HomeState extends State<Home> {
               hasBeenReleased = true;
             }
           },
-          child: Stack(
-            children: <Widget>[
-              Positioned(
-                top:0,
-                right:0,
-                child: CustomPaint(
-                  painter: CubePainter(scrambler.currentType,scramble),
-                  child: SizedBox(
-                    width: 300,
-                    height: 300,
-                  ),
-                ),
-              ),
-              Positioned(
-                top:0,
-                bottom:0,
-                left:0,
-                right:0,
-                child: Container(
-                  color: Colors.transparent,
-                )
-              ),
-              Positioned(
-                top: 10,
-                left:300,
-                right:300,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Wrap(
-                      children: <Widget>[
-                        ScrambleIcon(scrambler.currentType,"e222",CubingIcons.e222,changeScramble),
-                        ScrambleIcon(scrambler.currentType,"e333",CubingIcons.e333,changeScramble),
-                        ScrambleIcon(scrambler.currentType,"e444",CubingIcons.e444,changeScramble),
-                        ScrambleIcon(scrambler.currentType,"e555",CubingIcons.e555,changeScramble),
-                        ScrambleIcon(scrambler.currentType,"e666",CubingIcons.e666,changeScramble),
-                        ScrambleIcon(scrambler.currentType,"e777",CubingIcons.e777,changeScramble),
-                        ScrambleIcon(scrambler.currentType,"ePyram",CubingIcons.ePyram,changeScramble),
-                        ScrambleIcon(scrambler.currentType,"eMinx",CubingIcons.eMinx,changeScramble),
-                        ScrambleIcon(scrambler.currentType,"eSkewb",CubingIcons.eSkewb,changeScramble),
-                        ScrambleIcon(scrambler.currentType,"eClock",CubingIcons.eClock,changeScramble),
-                        ScrambleIcon(scrambler.currentType,"eSq1",CubingIcons.eSq1,changeScramble),
-                      ],
+          child: LayoutBuilder(
+            builder: (context,constraints)=>Stack(
+              children: <Widget>[
+                if(constraints.maxWidth>950)
+                  Positioned(
+                    top:0,
+                    right:0,
+                    child: CustomPaint(
+                      painter: CubePainter(scrambler.currentType,scramble),
+                      child: SizedBox(
+                        width: 300,
+                        height: 300,
+                      ),
                     ),
-                    Center(child: Text(scramble,style: TextStyle(fontSize: 20)))
-                  ],
+                  ),
+                Positioned(
+                  top:0,
+                  bottom:0,
+                  left:0,
+                  right:0,
+                  child: Container(
+                    color: Colors.transparent,
+                  )
                 ),
-              ),
-              Center(
-                child: Text(
-                  isInspecting ? (15-(inspectingStopwatch.elapsedMilliseconds/1000).floor()).toString() : specialMinutes(mainStopwatch.elapsedMilliseconds/1000.0),
-                  style: TextStyle(
-                    fontSize: 100,
-                    color: isFreezing ? (freezingStopwatch.elapsedMilliseconds>=freezeTime ? Colors.greenAccent[200] : Colors.red) : null,
+                Positioned(
+                  top: 10,
+                  left:constraints.maxWidth>650 ? 300 : 0,
+                  right:constraints.maxWidth>950 ? 300 : 0,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Wrap(
+                        children: <Widget>[
+                          ScrambleIcon(scrambler.currentType,"e222",CubingIcons.e222,changeScramble),
+                          ScrambleIcon(scrambler.currentType,"e333",CubingIcons.e333,changeScramble),
+                          ScrambleIcon(scrambler.currentType,"e444",CubingIcons.e444,changeScramble),
+                          ScrambleIcon(scrambler.currentType,"e555",CubingIcons.e555,changeScramble),
+                          ScrambleIcon(scrambler.currentType,"e666",CubingIcons.e666,changeScramble),
+                          ScrambleIcon(scrambler.currentType,"e777",CubingIcons.e777,changeScramble),
+                          ScrambleIcon(scrambler.currentType,"ePyram",CubingIcons.ePyram,changeScramble),
+                          ScrambleIcon(scrambler.currentType,"eMinx",CubingIcons.eMinx,changeScramble),
+                          ScrambleIcon(scrambler.currentType,"eSkewb",CubingIcons.eSkewb,changeScramble),
+                          ScrambleIcon(scrambler.currentType,"eClock",CubingIcons.eClock,changeScramble),
+                          ScrambleIcon(scrambler.currentType,"eSq1",CubingIcons.eSq1,changeScramble),
+                        ],
+                      ),
+                      Center(child: Text(scramble,style: TextStyle(fontSize: 20)))
+                    ],
                   ),
                 ),
-              )
-            ],
+                Center(
+                  child: Text(
+                    isInspecting ? (15-(inspectingStopwatch.elapsedMilliseconds/1000).floor()).toString() : specialMinutes(mainStopwatch.elapsedMilliseconds/1000.0),
+                    style: TextStyle(
+                      fontSize: 100,
+                      color: isFreezing ? (freezingStopwatch.elapsedMilliseconds>=freezeTime ? Colors.green : Colors.red) : null,
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
